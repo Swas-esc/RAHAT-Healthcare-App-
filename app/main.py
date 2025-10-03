845bc063a5ef9f18097c581513d1657b105690d8
Swas-esc <tripathisushila28@gmail.com>
2025-10-03 16:14:03 +0530

    Initial commit - setup project structure and hello world app

---
+++ app/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title("RAHAT - Rural Access to Healthcare")
+    st.subheader("Hellow, this is your first RAHAT app running!")
+    st.write("✅ Stramlit app setup is successful.")
+    
+if __name__ == "__main__":
+    main()
